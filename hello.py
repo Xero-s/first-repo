b5fe68fb7ddf908b35fa1b0c2e5528bdd770f0cd
Xero_s <bumiyam@gmail.com>
2022-05-06 13:30:58 +0900

    practice

---
+++ hello.py
@@ -0,0 +1,10 @@
+for i in range(1, 21):
+    if i % 3 == 0:
+        print("year")
+    elif i % 5 == 0:
+        print("dream")
+    elif i % 20 == 0:
+        print("yeardream")
+    else:
+        print(i)
+        
